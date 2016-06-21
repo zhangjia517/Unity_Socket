3f4cdd717fb01d01bc5acd399e0eb72571793c74
zhangjia517@hotmail.com <zhangjia517@hotmail.com>
2016-06-21 16:52:12 +0800

    LM

--- Server/NodejsServer/Server_Socket.js
+++ Server/NodejsServer/Server_Socket.js
@@ -1,51 +1,60 @@
 ﻿var net = require('net');  
-var chatServer = net.createServer(), clientList = [];
+var chatServer = net.createServer()
 var receiveNick = false;
+var clientList = [];
 var clientNick = '';
-      
+ 
 chatServer.on('connection', function(client) 
 {  
     console.log('welcome ' +  client.localAddress + ':'+ client.remotePort);
-    clientList.push(client);   
+    clientList.push(client);
+	receiveNick = false;
   
     client.on('data', function(data) {
 		if(!receiveNick)
 		{
 			clientNick = data;
-			broadcast(clientNick + ' has joined the chat', client);
+			broadcast(clientNick + ' has joined the chat');
 			receiveNick = true;
 		}
 		else
 		{
-			broadcast(clientNick + '>' + data, client);
+			broadcast(clientNick + '>' + data);
 		}
     });  
 	
-	client.on('end', function() {  
-    clientList.splice(clientList.indexOf(client), 1);
+	client.on('end', function() 
+	{  
+		clientList.splice(clientList.indexOf(client), 1);
 	}) 
 	
-    client.on('error', function(e) {  
-	console.log(e);  
+    client.on('error', function(e) 
+	{  
+		console.log(e);  
 	});
 });  
 
-function broadcast(message, client) {  
+function broadcast(message) 
+{  
 	var cleanup = []
     for(var i=0;i<clientList.length;i+=1) {      
-      if(clientList[i].writable) {
-        clientList[i].write(message + '\n')  
-      } else {  
-        cleanup.push(clientList[i]) 
-        clientList[i].destroy()  
-      }      
-    }    
+		if(clientList[i].writable) 
+		{
+			clientList[i].write(message + '\n')
+			console.log(message);
+		} else 
+		{  
+			cleanup.push(clientList[i]) 
+			clientList[i].destroy()  
+		}      
+    }
   	for(i=0;i<cleanup.length;i+=1) 
 	{  
 		clientList.splice(clientList.indexOf(cleanup[i]), 1)
 	}
 }  
 
-chatServer.listen(5819,'192.168.16.150', function() { 
-  console.log('server 9000 on on on');
+chatServer.listen(5819,'192.168.16.150', function() 
+{ 
+	console.log('server 9000 on on on');
 });
